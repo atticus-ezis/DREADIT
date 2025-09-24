e2ec188f97002b710babef497da6df28f7e7a0b8
Atticus Ezis <atticus.ezis@gmail.com>
2025-09-24 23:13:02 +0800

    Initial Commit
    Created django-project
    Added:
    gitignore
    .env
    postgresql url
    uv

---
+++ Dreadit/test.py
@@ -0,0 +1,12 @@
+import os
+import environ
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+env.read_env(BASE_DIR / ".env")
+
+
+url = env.str("DATABASE_URL")
+print(url)
